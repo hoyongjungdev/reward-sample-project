edf242c6194bd12698c976461fb15af87e2cc227
Hoyong Jung <hoyong.jung.dev@gmail.com>
2022-12-14 02:10:04 +0900

    [frontend] 메인 페이지에 보상 데이터 출력
    - Axios dependency 추가

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,15 +1,34 @@
 import Header from "./Header";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function Home() {
-    useEffect(() => {
+    const [notification, setNotification] = useState({
+        id: '',
+        title: '',
+        descriptions: []
+    });
 
+    useEffect(() => {
+        axios.get("http://localhost:8080/notifications")
+            .then(function (response) {
+                setNotification(response.data.notifications[0])
+            });
     }, []);
 
     return (
         <div>
             <Header/>
-            <p>hello, world!</p>
+            <h2>{notification.title}</h2>
+            <ul>
+                {notification.descriptions.map(
+                    (d, idx) => <li key={idx}>{d.line}
+                        <ul>{d.subline.map(
+                            (s, idx) => <li key={idx}>{s}</li>
+                        )}</ul>
+                    </li>
+                )}
+            </ul>
         </div>
     );
 }
