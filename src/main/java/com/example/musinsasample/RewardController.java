547d637e134f8d218c5bea5890dc78ad34b0d07b
Hoyong Jung <hoyong.jung.dev@gmail.com>
2022-12-14 02:08:36 +0900

    getNotifications API 추가
    - CORS 설정 추가

--- src/main/java/com/example/musinsasample/RewardController.java
+++ src/main/java/com/example/musinsasample/RewardController.java
@@ -4,13 +4,19 @@ import com.example.musinsasample.application.IssueRewardRequest;
 import com.example.musinsasample.application.SuccessResponse;
 import com.example.musinsasample.domain.service.RewardService;
 import com.example.musinsasample.infra.TimeProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 @RestController
@@ -33,5 +39,18 @@ public class RewardController {
         rewardService.issueReward(userId, today);
 
         return new SuccessResponse(true);
+    }
+
+    @GetMapping(path = "notifications", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Object getNotifications() {
+        Resource resource = new ClassPathResource("/static/notifications.json");
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(resource.getInputStream(), Object.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
     }
 }
